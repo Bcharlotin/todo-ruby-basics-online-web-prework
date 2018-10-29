0ccb65f0e3362dd409d3a10f4311660e63a49a4a
biancacharlotin <biancacharlotin@gmail.com>
2018-10-29 19:29:57 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,34 @@
 def division(num1, num2)
- 
+ num1=12 / num2=2 
 end
 
 def assign_variable(value)
+    value="Bob"
+end
+
+def argue(phrase)
+  puts phrase
+  return phrase
+
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting( greetings,  name )
 
 end
 
-def return_a_value
-
+def return_a_value(value2)
+     puts value2
+    return value2
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value1)
+  puts value1
+  return value1
 end
 
-def pizza_party
+def pizza_party(type="cheese")
+  puts type
+  return type
 
 end
